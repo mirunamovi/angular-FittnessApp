13edc4a5ffbd1f3d9f6a8462e148b306ba4a6542
Teodora <teodoraharabor7@gmail.com>
2023-08-01 23:17:58 +0300

    modificari footer

--- src/app/home-module/tab.component.ts
+++ src/app/home-module/tab.component.ts
@@ -5,36 +5,28 @@ import { MatTabChangeEvent } from '@angular/material/tabs';
   selector: 'app-tab',
   templateUrl: './tab.component.html',
   styleUrls: ['./tab.component.css'],
-  // standalone: true,
-  // imports: [
-  //   MatTabsModule,
-  //   MatIconModule,
-  //   MatButtonModule,
-  //   MatInputModule,
-  //   CommonModule,
-  //   SharedModule,
-  // ],
 })
-
 export class TabComponent {
   previousDay = 'Vineri';
   nextDay = 'Marti';
+  selectedDayIndex = 0;
   arr = ['Luni', 'Marti', 'Miercuri', 'Joi', 'Vineri', 'Rezumat'];
 
   onClick(value: number) {
-    this.previousDay = this.arr[value-1];
-    this.nextDay = this.arr[value+1];
+    console.log('Value:', value);
+    this.previousDay = this.arr[value - 1];
+    this.nextDay = this.arr[value + 1];
 
-    if(this.arr[value] == 'Rezumat'){
-      this.previousDay = this.arr[value-1];
+    this.selectedDayIndex = value;
+
+    if (this.arr[value] == 'Rezumat') {
+      this.previousDay = this.arr[value - 1];
       this.nextDay = this.arr[0];
     }
 
-    if(this.arr[value] == 'Luni'){
-      this.previousDay = this.arr[this.arr.length-1];
-      this.nextDay = this.arr[value+1];
+    if (this.arr[value] == 'Luni') {
+      this.previousDay = this.arr[this.arr.length - 1];
+      this.nextDay = this.arr[value + 1];
     }
-    console.log(this.previousDay);
-    console.log(this.nextDay);
   }
 }
