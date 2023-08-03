ec9a1da015d54e2194175816318a743d5b943972
PopescuStefan1 <stefan.ro95@gmail.com>
2023-08-03 11:48:06 +0300

    Update day comp

--- src/app/shared-module/day-component/day.component.ts
+++ src/app/shared-module/day-component/day.component.ts
@@ -1,18 +1,18 @@
 import { Component, Input } from '@angular/core';
 
-
-
 @Component({
   selector: 'app-day',
-  templateUrl: './day.component.html',
-  styleUrls: ['./day.component.css'],
-  // standalone: true,
-  // imports: [MatCardModule, MatButtonModule, ActivityComponent],
 
+  templateUrl: './day.component.html',
+
+  styleUrls: ['./day.component.css'],
+
+  // standalone: true,
+
+  // imports: [MatCardModule, MatButtonModule, ActivityComponent],
 })
 export class DayComponent {
   @Input() day = '';
+
   @Input() index!: number;
-
 }
-
